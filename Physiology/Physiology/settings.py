ee45c124e6ef465abcaaefefeaa93c961f7ca7fc
Luis Gabriel Venegas Saucedo <venegassaucedoluis@gmail.com>
2025-06-02 16:04:36 -0600

    primer push

--- Physiology/Physiology/settings.py
+++ Physiology/Physiology/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'Physiology.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +118,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [ BASE_DIR / STATIC_URL ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- Physiology/Physiology/urls.py
+++ Physiology/Physiology/urls.py
@@ -14,9 +14,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# physiology/physiology/Physiology/urls.py
 from django.contrib import admin
 from django.urls import path
+from store import views  # Importa tus vistas desde la aplicación 'store'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='home'),  # La página de inicio
+    path('shop/', views.shop, name='shop'),
+    path('skin-guide/', views.skin_guide, name='skin_guide'),
+    path('the-brand/', views.the_brand, name='the_brand'),
+    path('the-journaling/', views.the_journaling, name='the_journaling'),
+    path('contact/', views.contact, name='contact'),
+    path('faq/', views.faq, name='faq'),
+    path('shipping/', views.shipping, name='shipping'),
+    path('returns/', views.returns, name='returns'),
+    path('terms/', views.terms, name='terms'),
+    path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
+    path('partners/', views.partners, name='partners'),
+    path('find-distributor/', views.find_distributor, name='find_distributor'),
+    path('become-distributor/', views.become_distributor, name='become_distributor'),
+    path('benefits/', views.benefits, name='benefits'),
+    path('distribution-info/', views.distribution_info, name='distribution_info'),
+    path('minimalist-philosophy/', views.minimalist_philosophy, name='minimalist_philosophy'),
+    path('radiant-skin/', views.radiant_skin, name='radiant_skin'),
+    path('essential-only/', views.essential_only, name='essential_only'),
+    path('minimalist-philosophy-2/', views.minimalist_philosophy_2, name='minimalist_philosophy_2'),
 ]
